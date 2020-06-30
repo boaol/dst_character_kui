4e58709ba6ff059f5ae7f82f2ad3437e0139a67b
Boao Li <liboao1122@hotmail.com>
2020-06-30 14:04:54 +1000

    Update kuijiang.lua
     - fix linting issues

--- scripts/prefabs/kuijiang.lua
+++ scripts/prefabs/kuijiang.lua
@@ -1,26 +1,20 @@
 local MakePlayerCharacter = require "prefabs/player_common"
 
 local assets = {
-    Asset("SCRIPT", "scripts/prefabs/player_common.lua"),
+    Asset("SCRIPT", "scripts/prefabs/player_common.lua")
 }
 local prefabs = {}
 
--- 初始物品
-local start_inv = {
-}
+local start_inv = {}
 
--- 当人物复活的时候
 local function onbecamehuman(inst)
-	-- 设置人物的移速（1表示1倍于wilson）
-	inst.components.locomotor:SetExternalSpeedMultiplier(inst, "kuijiang_speed_mod", 1.2)
+    inst.components.locomotor:SetExternalSpeedMultiplier(inst, "kuijiang_speed_mod", 1.2)
 end
---当人物死亡的时候
 local function onbecameghost(inst)
-	-- 变成鬼魂的时候移除速度修正
-   inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "kuijiang_speed_mod")
+    inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "kuijiang_speed_mod")
 end
 
--- 重载游戏或者生成一个玩家的时候
+-- get called when reload game or a player respawn
 local function onload(inst)
     inst:ListenForEvent("ms_respawnedfromghost", onbecamehuman)
     inst:ListenForEvent("ms_becameghost", onbecameghost)
@@ -32,32 +26,29 @@ local function onload(inst)
     end
 end
 
---这个函数将在服务器和客户端都会执行
---一般用于添加小地图标签等动画文件或者需要主客机都执行的组件（少数）
-local common_postinit = function(inst) 
-	-- Minimap icon
-	inst:AddTag("kuijiang")
-	inst.MiniMapEntity:SetIcon( "kuijiang.tex" )
+local common_postinit = function(inst)
+    -- Minimap icon
+    inst:AddTag("kuijiang")
+    inst.MiniMapEntity:SetIcon("kuijiang.tex")
 end
 
--- 这里的的函数只在主机执行,一般组件之类的都写在这里
 local master_postinit = function(inst)
-	-- 人物音效
-	inst.soundsname = "wendy"
-	
-	inst.components.health:SetMaxHealth(TUNING.KUIJIANG_HEALTH)
-	inst.components.hunger:SetMax(TUNING.KUIJIANG_SANITY)
-	inst.components.sanity:SetMax(TUNING.KUIJIANG_HUNGER)
-	
-	-- 伤害系数
+    -- soundsname
+    inst.soundsname = "wendy"
+
+    inst.components.health:SetMaxHealth(TUNING.KUIJIANG_HEALTH)
+    inst.components.hunger:SetMax(TUNING.KUIJIANG_SANITY)
+    inst.components.sanity:SetMax(TUNING.KUIJIANG_HUNGER)
+
+    -- damagemultiplier
     inst.components.combat.damagemultiplier = 1.1
-	
-	-- 饥饿速度
-	inst.components.hunger.hungerrate = 1 * TUNING.WILSON_HUNGER_RATE
-	
-	inst.OnLoad = onload
+
+    -- hungerrate
+    inst.components.hunger.hungerrate = 1 * TUNING.WILSON_HUNGER_RATE
+
+    inst.OnLoad = onload
     inst.OnNewSpawn = onload
-	
+
 end
 
 return MakePlayerCharacter("kuijiang", prefabs, assets, common_postinit, master_postinit, start_inv)
